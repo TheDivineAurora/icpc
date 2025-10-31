77645c6de266e4b700daf863ae50d1e03adfeb94
Siddharthjoshi2624 <153123967+Siddharthjoshi2624@users.noreply.github.com>
2025-10-31 18:39:18 +0530

    Implement LCA structure with DFS and queries

---
+++ LCA.cpp
@@ -0,0 +1,51 @@
+const int N = 5e5;
+const int LN = 20;
+
+int dp[N + 5][LN];
+struct LCA {
+    vector<vector<int>> adj;
+    vector<int> d;
+    void dfs(int u, int p) {
+        dp[u][0] = p;
+        for (int i = 1; i < LN; ++i) {
+            dp[u][i] = dp[dp[u][i - 1]][i - 1];
+        }
+        for (auto v : adj[u]) if (v != p) {
+                d[v] = d[u] + 1;
+                dfs(v, u);
+            }
+    }
+
+    LCA(vector<vector<int>>& __adj) {
+        adj = __adj;
+        d.resize(adj.size());
+        dfs(1, 0);
+    }
+
+    int lca(int u, int v) {
+        if (d[u] < d[v]) swap(v, u);
+        int diff = d[u] - d[v];
+        for (int i = 0; i < LN; ++i) {
+            if (diff & (1 << i)) {
+                u = dp[u][i];
+            }
+        }
+        if (u == v) return u;
+        for (int i = LN - 1; i >= 0; --i) {
+            if (dp[u][i] != dp[v][i]) {
+                u = dp[u][i];
+                v = dp[v][i];
+            }
+        }
+        return dp[u][0];
+    }
+
+    int kth(int u, int dis) {
+        for (int i = 0; i < LN; ++i) {
+            if (dis & (1 << i)) {
+                u = dp[u][i];
+            }
+        }
+        return u;
+    }
+};
